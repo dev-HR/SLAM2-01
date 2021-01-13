177ae70012fd6a5f14dc3c3ff1009e807cd961e4
etudiant <etudiant@a104-poste1.sio.lla.fr>
2021-01-13 09:45:49 +0100

    Sources

---
+++ DateDuJour.java
@@ -0,0 +1,18 @@
+// Afficher la date du jour
+
+public class DateDuJour {
+	
+	public static void main(String [] args){
+		
+		int jour, annee ;
+		String mois ;
+		
+		jour = 15 ;
+		mois = "Janvier" ;
+		annee = 2020 ;
+		
+		System.out.println("Nous sommes le") ;
+		System.out.println(jour + " " + mois + " " + annee ) ;
+		
+	}
+}
